fc455fc01419593874e4d9c679889ac5a725fca2
Your Name <shtefanuk.dima@gmail.com>
2019-03-09 21:37:12 +0200

    first commit

---
+++ modul 3/script.js
@@ -0,0 +1,33 @@
+"use strict"
+
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+let UserLogin;
+
+const isLoginValid = function(logins) {
+  this.logins = logins;
+ if(addLogin < logins['']) {
+  alert('Ошибка! Логин должен быть от 4 до 16 символов')
+}else if(addLogin > logins[''] {
+  alert('Ошибка! Логин должен быть от 4 до 16 символов')
+
+}else(addLogin === logins['']) {
+
+}
+
+};
+
+const isLoginUnique = function(allLogins, login) {
+  // код
+};
+
+const addLogin = function(allLogins, login) {
+  // код
+};
+
+// Вызовы функции для проверки
+addLogin('Ajax'); // 'Логин успешно добавлен!'
+addLogin('robotGoogles'); // 'Такой логин уже используется!'
+addLogin('Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin('jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+// UserLogin = logins.add(addLogin)
+
